9f76a888c26f02b8d1fc1fdeeda72768475dd26d
Prity Das Bornita <114295389+prityd825@users.noreply.github.com>
2024-02-19 23:50:46 +0600

    Update settings.py

--- E-medical/settings.py
+++ E-medical/settings.py
@@ -142,6 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mahbubhasanmht2@gmail.com'
+EMAIL_HOST_USER = 'prityd825@gmail.com'
 EMAIL_HOST_PASSWORD = 'dsldqzybkncqyofo'
 
